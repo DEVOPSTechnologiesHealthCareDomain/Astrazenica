e6be38a0a3b12b4132105778a6d127a9e996280e
purna <purna.devops4u@gmail.com>
2019-01-13 23:38:52 +0530

    Reverting back -- Revert "added 2 files"
    This reverts commit 7f6b5fbfd70070238e97c636c02d76fe6d1194de.

--- Users.java
+++ Users.java
@@ -1,5 +1,3 @@
-Hi Guys, you pepoles need to pratice more and more, if you don't practice after two days you will forgot.....
-
-This java class contains user properites updated
+This java class contains user properites updated 
 Hi Guys
 Good Evening this is java class
