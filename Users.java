7f6b5fbfd70070238e97c636c02d76fe6d1194de
purna <purna.devops4u@gmail.com>
2019-01-13 23:31:10 +0530

    one file is Updated and added 2 files

--- Users.java
+++ Users.java
@@ -1,3 +1,5 @@
-This java class contains user properites updated 
+Hi Guys, you pepoles need to pratice more and more, if you don't practice after two days you will forgot.....
+
+This java class contains user properites updated
 Hi Guys
 Good Evening this is java class
